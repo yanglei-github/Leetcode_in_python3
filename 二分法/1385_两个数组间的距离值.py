eae3b281dd19cbbc1b01f55a8558f5751a40a49b
792415589@qq.com <792412589@qq.com>
2022-05-29 11:50:27 +0800

    fix some bugs

---
+++ 二分法/1385_两个数组间的距离值.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findTheDistanceValue(self, arr1, arr2, d):
+        ans=0
+        arr2.sort()
+        for i in range(len(arr1)):
+            left,right=0,len(arr2)-1
+            flag = 0
+            while left < right:
+                mid=(right+left+1) // 2
+                if arr2[mid] - arr1[i] > d:
+                    right=mid-1
+                elif arr2[mid] - arr1[i] < -d:
+                    left = mid + 1
+                else: 
+                    flag = 1
+                    break
+            if abs(arr2[right]-arr1[i]) > d and flag == 0:
+                ans+=1
+        return ans
--- 滑动窗口/930_和相同的二元子数组.py
+++ 滑动窗口/930_和相同的二元子数组.py
@@ -5,6 +5,29 @@ Created on Sun Jul 12 14:19:38 2020
 @author: leiya
 """
 
+
+'''
+20220430 前缀和+hash
+'''
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        adict = {}
+        adict[0] = 1
+        cur_sum = 0
+        count = 0
+        for num in nums:
+            cur_sum += num
+            #注意下面的两个if存在先后顺序
+            if cur_sum - goal in adict:
+                count += adict[cur_sum-goal]
+            if cur_sum not in adict:
+                adict[cur_sum] = 1
+            else:
+                adict[cur_sum] += 1
+        return count 
+    
+
+
 '''
 参考992，1248
 给定一个0，1数组，你可以选择最多S个1和任意个0，你的选择数减去 给定一个0，1数组，你可以选择最多S - 1个1和任意个0，你的选择数
